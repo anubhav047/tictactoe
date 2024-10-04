d6f020b50e5b2f76ee013033adaeca0bd237ec0c
Anubhav Goel <goelanubhav2003@gmail.com>
2024-10-05 04:11:47 +0530

    Initial Commit

---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by Anubhav Goel on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tictactoeTests/tictactoeTests.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeTests.swift
+//  tictactoeTests
+//
+//  Created by Anubhav Goel on 05/10/24.
+//
+
+import Testing
+@testable import tictactoe
+
+struct tictactoeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
